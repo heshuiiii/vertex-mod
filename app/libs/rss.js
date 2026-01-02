24258e295dcfe6b5793d70d050501884a69f1a27
heshuiiii <71701401+heshuiiii@users.noreply.github.com>
2026-01-02 14:41:52 +0800

    rsscachetimeedit

--- app/libs/rss.js
+++ app/libs/rss.js
@@ -50,7 +50,7 @@ const _getRssContent = async function (rssUrl, suffix = true) {
       body = '<?xml version="1.0" encoding="utf-8"?>\n' + res.body.match(/<rss[\s\S]*<\/rss>/)[0];
     }
     const host = new URL(rssUrl).host;
-    let cacheTime = ['lemon', 'hhanclub'].some(item => host.indexOf(item) !== -1) ? 150 : 40;
+    let cacheTime = ['lemon', 'hhanclub'].some(item => host.indexOf(item) !== -1) ? 150 : 2;
     if (host.indexOf('sharkpt') !== -1) {
       cacheTime = 310;
     }
